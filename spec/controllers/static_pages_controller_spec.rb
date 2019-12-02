9f5cc1259faea397e20e4528aad8af6cd3a15ca9
creatase <yukita_jp@creatase.info>
2019-12-02 19:18:54 +0900

    titleタグ内容のテストを追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+      expect(response.body).to have_title 'Home | naeatta'
     end
   end
 
@@ -13,6 +15,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns http success" do
       get :help
       expect(response).to have_http_status(:success)
+      expect(response.body).to have_title 'Help | naeatta'
     end
   end
 
@@ -20,6 +23,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns http success" do
       get :about
       expect(response).to have_http_status(:success)
+      expect(response.body).to have_title 'About | naeatta'
     end
   end
 
