f7e81fdb0b836e448ad1660d2c8f72f0ebf52b90
creatase <yukita_jp@creatase.info>
2019-12-03 09:11:25 +0900

    root about help contact のsystem specを追加

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,10 +1,36 @@
 require "rails_helper"
 
 RSpec.describe "Static Pages", :type => :system do
+  let(:base_title) { 'naeatta' }
+
+  describe 'root' do
+    it 'Home画面の表示' do
+      visit '/'
+      expect(page).to have_title "Home | #{ base_title }"
+    end
+  end
   describe 'Home' do
-    specify '画面の表示' do
+    it '画面の表示' do
       visit '/static_pages/home'
-      expect(page).to have_css('h1', text: 'Sample App')
+      expect(page).to have_title "Home | #{ base_title }"
+    end
+  end
+  describe 'About' do
+    it '画面の表示' do
+      visit '/static_pages/about'
+      expect(page).to have_title "About | #{ base_title }"
+    end
+  end
+  describe 'Help' do
+    it '画面の表示' do
+      visit '/static_pages/help'
+      expect(page).to have_title "Help | #{ base_title }"
+    end
+  end
+  describe 'Contact' do
+    it '画面の表示' do
+      visit '/static_pages/contact'
+      expect(page).to have_title "Contact | #{ base_title }"
     end
   end
 end
