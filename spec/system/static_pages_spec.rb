258994519a7c25ce76dfb67e0f4147244554f3a2
creatase <yukita_jp@creatase.info>
2019-12-04 13:15:14 +0900

    各ページへのリンクテスト追加

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -3,12 +3,24 @@ require "rails_helper"
 RSpec.describe "Static Pages", type: :system do
   let(:base_title) { 'naeatta' }
 
+  describe 'ナビゲーション' do
+    before do
+      visit '/'
+    end
+    it '各ページへのリンクがある' do
+      expect(page).to have_link nil, href: root_path, count: 2
+      expect(page).to have_link 'Help', href: help_path
+      expect(page).to have_link 'About', href: about_path
+      expect(page).to have_link 'Contact', href: contact_path
+    end
+  end
+
   describe 'root' do
     it 'Home画面の表示' do
       visit '/'
       expect(page).to have_title base_title
       expect(page).to_not have_title "Home |"
-    end
+      end
   end
   describe 'About' do
     it '画面の表示' do
