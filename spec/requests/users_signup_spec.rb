3e671ff6e1dd41ad579979d65d2ee370af432361
creatase <yukita_jp@creatase.info>
2019-12-05 20:47:12 +0900

    user 登録成功時のテストを追加

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -4,11 +4,34 @@ RSpec.describe "Users Signup", type: :request do
   example '無効な情報で登録に失敗すると登録画面を表示する' do
     get signup_path
     expect{
-      post users_path, params: { user: { name:  "",
-        email: "user@invalid",
-        password:              "foo",
-        password_confirmation: "bar" } }
-      }.to change{User.count}.by(0)
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+          }
+        }
+      }.to change{ User.count }.by(0)
     expect(response).to render_template(:new)
+  end
+
+  example '有効な情報で登録に成功すると詳細画面を表示する' do
+    get signup_path
+    expect{
+      post users_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    }.to change{User.count}.by(1)
+    expect(response).to redirect_to(user_path(User.last))
+    follow_redirect!
+
+    expect(response).to render_template(:show)
+
   end
 end
--- spec/system/users_signup_spec.rb
+++ spec/system/users_signup_spec.rb
@@ -22,4 +22,20 @@ RSpec.describe "Users Signup", type: :system do
       expect(page).to have_content "Password is too short (minimum is 6 characters)"
     end
   end
+
+  describe '有効な情報で登録ボタンを押下する' do
+    it 'ユーザー詳細画面が表示される' do
+      visit signup_path
+      fill_in 'Name', with: 'Example User'
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Confirmation', with: 'password'
+
+      click_button '登録'
+      user = User.last
+
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content 'Welcome to the Sample App!'
+    end
+  end
 end
