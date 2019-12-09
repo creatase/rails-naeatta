79d0455d9793e37fcc8862f620449c7b14a8ebd0
creatase <yukita_jp@creatase.info>
2019-12-09 15:45:03 +0900

    ログアウト時のユーザー情報更新のテストを追加

--- spec/requests/users_edit_spec.rb
+++ spec/requests/users_edit_spec.rb
@@ -1,38 +1,72 @@
 require "rails_helper"
 
 RSpec.describe "Users Edit", type: :request do
-  let(:user) { FactoryBot.create(:user) }
+  let!(:user) { FactoryBot.create(:user) }
+  let(:name) { 'Foo Bar' }
+  let(:email) { 'foo@bar.com' }
 
-  example '有効な情報の場合、更新に成功する' do
-    get edit_user_path(user)
-    expect(response).to render_template(:edit)
-    name = 'Foo Bar'
-    email = 'foo@bar.com'
-    patch user_path(user), params: {
-      user: {
-        name: name,
-        email: email,
-        password: '',
-        password_confirmation: ''
+  describe 'ログイン時' do
+
+    before do
+      log_in_as(user)
+      get edit_user_path(user)
+    end
+
+    example '有効な情報の場合、更新に成功する' do
+      expect(response).to render_template(:edit)
+      patch user_path(user), params: {
+        user: {
+          name: name,
+          email: email,
+          password: '',
+          password_confirmation: ''
+        }
       }
-    }
-    expect(response).to redirect_to(user_path(user))
-    user.reload
-    expect(user.name).to eq name
-    expect(user.email).to eq email
+      expect(response).to redirect_to(user_path(user))
+      user.reload
+      expect(user.name).to eq name
+      expect(user.email).to eq email
+    end
+
+    example '無効な情報の場合、更新に失敗する' do
+      expect(response).to render_template(:edit)
+      patch user_path(user), params: {
+        user: {
+          name: '',
+          email: 'foo@invalid',
+          password: 'foo',
+          password_confirmation: 'bar'
+        }
+      }
+      expect(response).to render_template(:edit)
+    end
   end
 
-  example '無効な情報の場合、更新に失敗する' do
-    get edit_user_path(user)
-    expect(response).to render_template(:edit)
-    patch user_path(user), params: {
-      user: {
-        name: '',
-        email: 'foo@invalid',
-        password: 'foo',
-        password_confirmation: 'bar'
+  describe 'ログアウト時' do
+
+    before do
+      delete logout_path
+    end
+
+    example 'ユーザー編集画面へのアクセスはログイン画面へリダイレクトする' do
+      get edit_user_path(user)
+      expect(response).to redirect_to(login_path)
+      follow_redirect!
+      expect(response).to render_template(:new)
+    end
+
+    example 'ユーザー情報更新のpostはログイン画面へリダイレクトする' do
+      patch user_path(user), params: {
+        user: {
+          name: name,
+          email: email,
+          password: '',
+          password_confirmation: ''
+        }
       }
-    }
-    expect(response).to render_template(:edit)
+      expect(response).to redirect_to(login_path)
+      follow_redirect!
+      expect(response).to render_template(:new)
+    end
   end
 end
--- spec/system/users_edit_spec.rb
+++ spec/system/users_edit_spec.rb
@@ -3,21 +3,30 @@ require "rails_helper"
 RSpec.describe "Users Edit", type: :system do
   let(:user) { FactoryBot.create(:user) }
 
-  example '有効な情報の場合、更新が成功する' do
-    visit login_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'ログイン'
+  describe 'ログイン時' do
+    example '有効な情報の場合、更新が成功する' do
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'ログイン'
 
-    visit edit_user_path(user)
-    fill_in 'Name', with: 'Foo Bar'
-    fill_in 'Email', with: 'foo@bar.com'
-    fill_in 'Password', with: ''
-    fill_in 'Confirmation', with: ''
-    click_button '更新'
+      visit edit_user_path(user)
+      fill_in 'Name', with: 'Foo Bar'
+      fill_in 'Email', with: 'foo@bar.com'
+      fill_in 'Password', with: ''
+      fill_in 'Confirmation', with: ''
+      click_button '更新'
 
-    expect(current_path).to eq user_path(user)
-    expect(page).to have_content 'Profile updated'
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content 'Profile updated'
+    end
+  end
 
+  describe 'ログアウト時' do
+    example 'ログイン画面にリダイレクトする' do
+      visit edit_user_path(user)
+      expect(current_path).to eq login_path
+      expect(page).to have_content 'ログインしてください'
+    end
   end
 end
