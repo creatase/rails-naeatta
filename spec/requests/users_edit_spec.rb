0e690eb527b405607b7181453e55b0517fbb977b
creatase <yukita_jp@creatase.info>
2019-12-10 17:32:37 +0900

    リファクタリング　rubocop対応

--- spec/requests/users_edit_spec.rb
+++ spec/requests/users_edit_spec.rb
@@ -2,25 +2,24 @@ require "rails_helper"
 
 RSpec.describe "Users Edit", type: :request do
   let!(:user) { FactoryBot.create(:user) }
-  let(:name) { 'Foo Bar' }
-  let(:email) { 'foo@bar.com' }
+  let(:name) { "Foo Bar" }
+  let(:email) { "foo@bar.com" }
 
-  describe 'ログイン時' do
-
+  describe "ログイン時" do
     before do
       log_in_as(user)
       get edit_user_path(user)
     end
 
-    example '有効な情報の場合、更新に成功する' do
+    example "有効な情報の場合、更新に成功する" do
       expect(response).to render_template(:edit)
       patch user_path(user), params: {
         user: {
           name: name,
           email: email,
-          password: '',
-          password_confirmation: ''
-        }
+          password: "",
+          password_confirmation: "",
+        },
       }
       expect(response).to redirect_to(user_path(user))
       user.reload
@@ -28,37 +27,36 @@ RSpec.describe "Users Edit", type: :request do
       expect(user.email).to eq email
     end
 
-    example '無効な情報の場合、更新に失敗する' do
+    example "無効な情報の場合、更新に失敗する" do
       expect(response).to render_template(:edit)
       patch user_path(user), params: {
         user: {
-          name: '',
-          email: 'foo@invalid',
-          password: 'foo',
-          password_confirmation: 'bar'
-        }
+          name: "",
+          email: "foo@invalid",
+          password: "foo",
+          password_confirmation: "bar",
+        },
       }
       expect(response).to render_template(:edit)
     end
   end
 
-  describe 'ログアウト時' do
-
-    example 'ユーザー編集画面へのアクセスはログイン画面へリダイレクトする' do
+  describe "ログアウト時" do
+    example "ユーザー編集画面へのアクセスはログイン画面へリダイレクトする" do
       get edit_user_path(user)
       expect(response).to redirect_to(login_path)
       follow_redirect!
       expect(response).to render_template(:new)
     end
 
-    example 'ユーザー情報更新のpostはログイン画面へリダイレクトする' do
+    example "ユーザー情報更新のpostはログイン画面へリダイレクトする" do
       patch user_path(user), params: {
         user: {
           name: name,
           email: email,
-          password: '',
-          password_confirmation: ''
-        }
+          password: "",
+          password_confirmation: "",
+        },
       }
       expect(response).to redirect_to(login_path)
       follow_redirect!
@@ -66,39 +64,37 @@ RSpec.describe "Users Edit", type: :request do
     end
   end
 
-  describe '異なるユーザーでログイン' do
-
+  describe "異なるユーザーでログイン" do
     before do
-      @user_a = FactoryBot.create(:user, name: 'user_a', email: 'user_a@mail.com')
-      @user_b = FactoryBot.create(:user, name: 'user_b', email: 'user_b@mail.com')
+      @user_a = FactoryBot.create(:user, name: "user_a", email: "user_a@mail.com")
+      @user_b = FactoryBot.create(:user, name: "user_b", email: "user_b@mail.com")
       log_in_as(@user_a)
     end
 
-    example '別ユーザーの編集ページへのアクセスはホーム画面へリダイレクトされる' do
+    example "別ユーザーの編集ページへのアクセスはホーム画面へリダイレクトされる" do
       get edit_user_path(@user_b)
       expect(response).to redirect_to(root_path)
       follow_redirect!
       expect(response).to render_template(:home)
     end
 
-    example '別ユーザー情報更新のpostはホーム画面へリダイレクトする' do
+    example "別ユーザー情報更新のpostはホーム画面へリダイレクトする" do
       patch user_path(@user_b), params: {
         user: {
           name: name,
           email: email,
-          password: '',
-          password_confirmation: ''
-        }
+          password: "",
+          password_confirmation: "",
+        },
       }
       expect(response).to redirect_to(root_path)
       follow_redirect!
       expect(response).to render_template(:home)
     end
-
   end
 
-  describe 'フレンドリーフォアーディング' do
-    example 'ログアウト状態で編集画面へアクセスしリダイレクトしたログイン画面でログインすると編集画面へリダイレクトする' do
+  describe "フレンドリーフォアーディング" do
+    example "ログアウト状態で編集画面へアクセスしリダイレクトしたログイン画面でログインすると編集画面へリダイレクトする" do
       get edit_user_path(user)
       expect(response).to redirect_to login_path
       follow_redirect!
@@ -110,9 +106,9 @@ RSpec.describe "Users Edit", type: :request do
         user: {
           name: name,
           email: email,
-          password: '',
-          password_confirmation: ''
-        }
+          password: "",
+          password_confirmation: "",
+        },
       }
       log_in_as(user)
       expect(response).to redirect_to user_path(user)
