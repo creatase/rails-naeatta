61754455ab8057c2fe2ff29aa4b5711e4bcc9121
creatase <yukita_jp@creatase.info>
2019-12-11 12:51:52 +0900

    ユーザー削除機能を追加

---
+++ spec/requests/users_delete_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "Users Delete", type: :request do
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user, name: "Sterling Archer", email: "duchess@example.gov") }
+
+  before do
+    user.update_attribute(:admin, true)
+  end
+
+  it "ログアウトではログイン画面にリダイレクトされる" do
+    expect {
+      delete user_path(user)
+    }.to change { User.count }.by(0)
+    expect(response).to redirect_to(login_path)
+  end
+
+  it "管理者意外のログインではホーム画面にリダイレクトされる" do
+    log_in_as(other_user)
+    expect {
+      delete user_path(user)
+    }.to change { User.count }.by(0)
+    expect(response).to redirect_to(root_path)
+  end
+
+  it "管理者ログインではユーザーを削除できる" do
+    log_in_as(user)
+    expect {
+      delete user_path(other_user)
+    }.to change { User.count }.by(-1)
+    expect(response).to redirect_to(users_url)
+  end
+end
--- spec/system/users_index_spec.rb
+++ spec/system/users_index_spec.rb
@@ -2,27 +2,55 @@ require "rails_helper"
 
 RSpec.describe "Users Index", type: :system do
   before do
-    FactoryBot.create(:user, name: "Michael Example", email: "michael@example.com")
-    FactoryBot.create(:user, name: "Sterling Archer", email: "duchess@example.gov")
+    @admin = FactoryBot.create(:user, name: "Michael Example", email: "michael@example.com")
+    @non_admin = FactoryBot.create(:user, name: "Sterling Archer", email: "duchess@example.gov")
     FactoryBot.create(:user, name: "Lana Kane", email: "hands@example.gov")
     FactoryBot.create(:user, name: "Malory Archer", email: "boss@example.gov")
 
     30.times do |i|
       FactoryBot.create(:user, name: "User #{i}", email: "user-#{i}@example.com")
     end
+    @admin.update_attribute(:admin, true)
   end
 
-  example "ユーザー一覧ページにページネーションがある" do
-    login_user = User.first
-    visit login_path
-    fill_in "Email", with: login_user.email
-    fill_in "Password", with: "password"
-    click_button "ログイン"
+  describe "管理者ログイン" do
+    before do
+      visit login_path
+      fill_in "Email", with: @admin.email
+      fill_in "Password", with: "password"
+      click_button "ログイン"
+      visit users_path
+    end
 
-    visit users_path
-    expect(page).to have_css ".pagination"
-    User.paginate(page: 1).each do |user|
-      expect(page).to have_selector "a", text: user.name
+    example "ユーザー一覧ページにページネーションがある" do
+      expect(page).to have_css ".pagination"
+    end
+
+    example "各ユーザー詳細ページへのリンクと管理者意外のユーザーの削除リンクがある" do
+      User.paginate(page: 1).each do |user|
+        expect(page).to have_selector "a", text: user.name
+        unless user == @admin
+          expect(page).to have_link "削除", href: user_path(user)
+        end
+      end
+    end
+
+    example "管理者意外のユーザーを削除できる" do
+      expect {
+        first(".users li").click_link "削除"
+      }.to change { User.count }.by(-1)
+    end
+  end
+
+  describe "管理者意外でログイン" do
+    example "削除リンクが表示されない" do
+      visit login_path
+      fill_in "Email", with: @non_admin.email
+      fill_in "Password", with: "password"
+      click_button "ログイン"
+
+      visit users_path
+      expect(page).to_not have_selector "a", text: "削除"
     end
   end
 end
