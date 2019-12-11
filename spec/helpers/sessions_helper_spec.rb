5dd901998a7876ae8f659231ab8200b011cf0f9f
creatase <yukita_jp@creatase.info>
2019-12-11 17:31:14 +0900

    rubocop対応

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 # Specs in this file have access to a helper object that includes
 # the SessionsHelper. For example:
@@ -11,24 +11,20 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe SessionsHelper, type: :helper do
-
-  describe 'ログイン状態がcookieのみに保持されている場合' do
-
+  describe "ログイン状態がcookieのみに保持されている場合" do
     before do
       @user = FactoryBot.build(:user)
       remember(@user)
     end
 
-    it 'current_userメソッドがログインユーザーを返す' do
+    it "current_userメソッドがログインユーザーを返す" do
       expect(current_user).to eq @user
       expect(is_logged_in?).to be_falsey
     end
 
-    it '記憶ダイジェストが無効な場合current_userはnilを返す' do
+    it "記憶ダイジェストが無効な場合current_userはnilを返す" do
       @user.update_attribute(:remember_digest, User.digest(User.new_token))
       expect(current_user).to be_nil
     end
   end
-
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,110 +1,110 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   let(:user) do
-     User.new(
-       name: 'Example User',
-       email: 'user@example.com',
-       password: 'foobar',
-       password_confirmation: 'foobar'
-      )
+    User.new(
+      name: "Example User",
+      email: "user@example.com",
+      password: "foobar",
+      password_confirmation: "foobar",
+    )
   end
-  describe 'バリデーション' do
 
-    describe 'params nameが' do
-      context 'あるとき' do
-        it 'パスする' do
+  describe "バリデーション" do
+    describe "params nameが" do
+      context "あるとき" do
+        it "パスする" do
           expect(user.valid?).to be_truthy
         end
       end
-      context '空文字のとき' do
-        it 'パスしない' do
-          user.name = ''
+      context "空文字のとき" do
+        it "パスしない" do
+          user.name = ""
           expect(user.valid?).to be_falsey
         end
       end
-      context 'スペースのみのとき' do
-        it 'パスしない' do
-          user.name = '      '
+      context "スペースのみのとき" do
+        it "パスしない" do
+          user.name = "      "
           expect(user.valid?).to be_falsey
         end
       end
-      context '51文字以上のとき' do
-        it 'パスしない' do
-          user.name = 'a' * 51
+      context "51文字以上のとき" do
+        it "パスしない" do
+          user.name = "a" * 51
           expect(user.valid?).to be_falsey
         end
       end
-      context '50文字以下のとき' do
-        it 'パスする' do
-          user.name = 'a' * 50
+      context "50文字以下のとき" do
+        it "パスする" do
+          user.name = "a" * 50
           expect(user.valid?).to be_truthy
         end
       end
     end
 
-    describe 'params emailが' do
-      context 'あるとき' do
-        it 'パスする' do
+    describe "params emailが" do
+      context "あるとき" do
+        it "パスする" do
           expect(user.valid?).to be_truthy
         end
       end
-      context '空文字のとき' do
-        it 'パスしない' do
-          user.email = ''
+      context "空文字のとき" do
+        it "パスしない" do
+          user.email = ""
           expect(user.valid?).to be_falsey
         end
       end
-      context 'スペースのみのとき' do
-        it 'パスしない' do
-          user.email = '      '
+      context "スペースのみのとき" do
+        it "パスしない" do
+          user.email = "      "
           expect(user.valid?).to be_falsey
         end
       end
-      context '256文字以上のとき' do
-        it 'パスしない' do
-          user.email = 'a' * (256 - '@example.com'.length) + '@example.com'
+      context "256文字以上のとき" do
+        it "パスしない" do
+          user.email = "a" * (256 - "@example.com".length) + "@example.com"
           expect(user.valid?).to be_falsey
         end
       end
-      context '255文字以下のとき' do
-        it 'パスする' do
-          user.email = 'a' * (255 - '@example.com'.length) + '@example.com'
+      context "255文字以下のとき" do
+        it "パスする" do
+          user.email = "a" * (255 - "@example.com".length) + "@example.com"
           expect(user.valid?).to be_truthy
         end
       end
-      context '有効なアドレスのとき' do
-        it 'パスする' do
-          user.email = 'user@example.com'
+      context "有効なアドレスのとき" do
+        it "パスする" do
+          user.email = "user@example.com"
           expect(user.valid?).to be_truthy
-          user.email = 'USER@foo.COM'
+          user.email = "USER@foo.COM"
           expect(user.valid?).to be_truthy
-          user.email = 'A_US-ER@foo.bar.org'
+          user.email = "A_US-ER@foo.bar.org"
           expect(user.valid?).to be_truthy
-          user.email = 'first.last@foo.jp'
+          user.email = "first.last@foo.jp"
           expect(user.valid?).to be_truthy
-          user.email = 'alice+bob@baz.cn'
+          user.email = "alice+bob@baz.cn"
           expect(user.valid?).to be_truthy
         end
       end
-      context '無効なアドレスのとき' do
-        it 'パスしない' do
-          user.email = 'user@example,com'
+      context "無効なアドレスのとき" do
+        it "パスしない" do
+          user.email = "user@example,com"
           expect(user.valid?).to be_falsey
-          user.email = 'user_at_foo.org'
+          user.email = "user_at_foo.org"
           expect(user.valid?).to be_falsey
-          user.email = 'user.name@example.'
+          user.email = "user.name@example."
           expect(user.valid?).to be_falsey
-          user.email = 'foo@bar_baz.com'
+          user.email = "foo@bar_baz.com"
           expect(user.valid?).to be_falsey
-          user.email = 'foo@bar+baz.com'
+          user.email = "foo@bar+baz.com"
           expect(user.valid?).to be_falsey
-          user.email = 'foo@bar..com'
+          user.email = "foo@bar..com"
           expect(user.valid?).to be_falsey
         end
       end
-      context 'すでに使用されているとき' do
-        it 'パスしない' do
+      context "すでに使用されているとき" do
+        it "パスしない" do
           duplicate_user = user.dup
           duplicate_user.email = user.email.upcase
           user.save!
@@ -113,39 +113,39 @@ RSpec.describe User, type: :model do
       end
     end
 
-    describe 'password password_confirmationが' do
-      context '空文字のとき' do
-        it 'パスしない' do
-          user.password = user.password_confirmation = ' ' * 6
+    describe "password password_confirmationが" do
+      context "空文字のとき" do
+        it "パスしない" do
+          user.password = user.password_confirmation = " " * 6
           expect(user.valid?).to be_falsey
         end
       end
-      context '5文字以下のとき' do
-        it 'パスしない' do
-          user.password = user.password_confirmation = 'a' * 5
+      context "5文字以下のとき" do
+        it "パスしない" do
+          user.password = user.password_confirmation = "a" * 5
           expect(user.valid?).to be_falsey
         end
       end
-      context '6文字以上のとき' do
-        it 'パスする' do
-          user.password = user.password_confirmation = 'a' * 6
+      context "6文字以上のとき" do
+        it "パスする" do
+          user.password = user.password_confirmation = "a" * 6
           expect(user.valid?).to be_truthy
         end
       end
     end
   end
 
-  describe '登録内容' do
-    it 'emailは小文字に変換されていること' do
-      user.email = 'Foo@ExAMPle.CoM'
+  describe "登録内容" do
+    it "emailは小文字に変換されていること" do
+      user.email = "Foo@ExAMPle.CoM"
       user.save!
-      expect(user.reload.email).to eq 'foo@example.com'
+      expect(user.reload.email).to eq "foo@example.com"
     end
   end
 
-  describe '認証' do
-    it 'cookies[:remember_token]がnilの状態ではauthenticated?はfalseを返す' do
-      expect(user.authenticated?('')).to be_falsey
+  describe "認証" do
+    it "cookies[:remember_token]がnilの状態ではauthenticated?はfalseを返す" do
+      expect(user.authenticated?(:remember, "")).to be_falsey
     end
   end
 end
