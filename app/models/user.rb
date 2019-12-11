ef6ad82e4fb3f3bb88502271a9520cbb44961126
creatase <yukita_jp@creatase.info>
2019-12-11 14:35:15 +0900

    create_activation_digest を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,
             presence: true,
             length: { maximum: 50 }
@@ -49,5 +50,11 @@ class User < ApplicationRecord
     # メールアドレスを全て小文字にする
     def downcase_email
       email.downcase!
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
