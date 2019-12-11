a0487db8aaca2f1a3cdc8d09998bc55bab305808
creatase <yukita_jp@creatase.info>
2019-12-11 16:19:06 +0900

    account_activation メールのテストを追加

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,19 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq(["michael@example.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.parts[1].body.raw_source).to match("こんにちは#{user.name}さん")
+      expect(mail.body.parts[1].body.raw_source).to match(user.activation_token)
+      expect(mail.body.parts[1].body.raw_source).to match(CGI.escape(user.email))
     end
   end
 
@@ -21,7 +24,7 @@ RSpec.describe UserMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Password reset")
       expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
 
     it "renders the body" do
