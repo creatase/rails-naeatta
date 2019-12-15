b349f5b2865defb00563f879374d7f29a5d167c3
creatase <yukita_jp@creatase.info>
2019-12-15 14:07:42 +0900

    コメントを追加

--- spec/factories/seedlingsposts.rb
+++ spec/factories/seedlingsposts.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :seedlingspost do
-    item { "MyText" }
-    product_regulation { "MyText" }
-    shipping_date { "2019-12-15" }
-    scion { "MyText" }
-    rootstock { "MyText" }
-    count { 1 }
-    location { "MyText" }
-    order_unit { 1 }
-    remarks { "MyText" }
+    item { "MyText" } # 品目
+    product_regulation { "MyText" } # 規格
+    shipping_date { "2019-12-15" } # 出荷日
+    scion { "MyText" } # 穂木
+    rootstock { "MyText" } # 台木
+    count { 1 } # 本数
+    location { "MyText" } # 生産地
+    order_unit { 1 } # 発注単位
+    remarks { "MyText" } # 備考
     user { nil }
   end
 end
