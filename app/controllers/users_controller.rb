a4a5014a9f2e5f3589ab106af8041449d6ebd630
creatase <yukita_jp@creatase.info>
2019-12-16 13:07:41 +0900

    user詳細画面にseedlingspostsを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @seedlingsposts = @user.seedlingsposts.paginate(page: params[:page])
   end
 
   def new
