99b210de7eef6eaa2a2eeae5bfe880dab0443c18
creatase <yukita_jp@creatase.info>
2019-12-20 09:46:35 +0900

    usersテーブルにiconカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_025341) do
+ActiveRecord::Schema.define(version: 2019_12_19_232840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_025341) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "icon"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
