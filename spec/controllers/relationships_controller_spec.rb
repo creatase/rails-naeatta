c91df8d59966b6bbab9d61a87a078824d12358d3
creatase <yukita_jp@creatase.info>
2019-12-22 20:29:30 +0900

    フォロー／フォロー解除機能を実装

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,4 +1,22 @@
 require "rails_helper"
 
 RSpec.describe RelationshipsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user, name: "Sterling Archer", email: "duchess@example.gov") }
+  let(:follow) { FactoryBot.create(:relationship, follower_id: user.id, followed_id: other_user.id) }
+
+  it "フォローにはログインが必要" do
+    expect {
+      post :create
+    }.to change { Relationship.count }.by(0)
+    expect(response).to redirect_to login_path
+  end
+
+  it "フォロー解除にはログインが必要" do
+    follow
+    expect {
+      delete :destroy, params: { id: follow.id }
+    }.to change { Relationship.count }.by(0)
+    expect(response).to redirect_to login_path
+  end
 end
