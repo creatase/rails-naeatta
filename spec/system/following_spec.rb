7e42598aa7d4aaaf361206655cf7ad798be7b6e0
creatase <yukita_jp@creatase.info>
2019-12-22 20:47:43 +0900

    フォロー／フォロー解除のテストを追加

--- spec/system/following_spec.rb
+++ spec/system/following_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "Following", type: :system do
   let(:user) { FactoryBot.create(:user) }
   let(:other_user) { FactoryBot.create(:user, name: "Sterling Archer", email: "duchess@example.gov") }
-  let!(:follow) { FactoryBot.create(:relationship, follower_id: user.id, followed_id: other_user.id) }
-  let!(:followed) { FactoryBot.create(:relationship, follower_id: other_user.id, followed_id: user.id) }
+  let(:follow) { FactoryBot.create(:relationship, follower_id: user.id, followed_id: other_user.id) }
+  let(:followed) { FactoryBot.create(:relationship, follower_id: other_user.id, followed_id: user.id) }
 
   before do
     visit login_path
@@ -15,6 +15,7 @@ RSpec.describe "Following", type: :system do
 
   describe "フォロー中一覧画面" do
     example "フォロー中のユーザーが表示される" do
+      follow
       visit following_user_path(user)
       expect(user.following).to_not be_empty
       expect(page).to have_link user.following.count.to_s + "following"
@@ -26,6 +27,7 @@ RSpec.describe "Following", type: :system do
 
   describe "フォロワー一覧画面" do
     example "フォロワーが表示される" do
+      followed
       visit followers_user_path(user)
       expect(user.followers).to_not be_empty
       expect(page).to have_link user.followers.count.to_s + "followers"
@@ -33,5 +35,20 @@ RSpec.describe "Following", type: :system do
         expect(page).to have_link follower.name, href: user_path(follower)
       end
     end
+  end
+
+  example "フォローできる" do
+    visit user_path(other_user)
+    expect {
+      click_button "フォローする"
+    }.to change { user.following.count }.by(1)
+  end
+
+  example "フォロー解除できる" do
+    follow
+    visit user_path(other_user)
+    expect {
+      click_button "フォローを解除"
+    }.to change { user.following.count }.by(-1)
   end
 end
