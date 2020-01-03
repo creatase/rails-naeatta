3cb4ec762b413faa102c112b6d98839bb0e956ef
creatase <yukita_jp@creatase.info>
2020-01-03 15:28:24 +0900

    フォームを別画面に入分けたので投稿のインスタンス変数を削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @seedlingspost = current_user.seedlingsposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
